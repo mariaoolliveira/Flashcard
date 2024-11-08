959b1e6d4a841a0ab9418c12fc126abebc92928e
Maria Eduarda <madurodriguesoli@gmail.com>
2024-11-08 14:44:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química.',
+    'Qual é a fórmula química da água?',
+    'H²O, A água é composta por dois átomos de hidrogênio (H) e um átomo de oxigênio (O).'
+)
+
+criaCartao(
+    'História.',
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    'A guerra terminou com a rendição da Alemanha em maio e do Japão em setembro de 1945.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "O Pequeno Príncipe"?',
+    'Antoine de Saint-Exupéry.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome do continente onde está localizado o Deserto do Saara?',
+    'O Saara é o maior deserto quente do mundo, localizado no norte da África.'
+)
